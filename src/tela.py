7d78ff80d90759443a9070ffd1f5c122a2874b89
Marcos Gabriel Tavares <marcosgabriel.ro@hotmail.com>
2021-08-09 20:08:46 -0300

    random dfs roughly implemented

--- src/tela.py
+++ src/tela.py
@@ -1,5 +1,8 @@
 import pygame
 
+import networkx as nx
+
+
 try:
     pygame.init()
 except:
@@ -11,6 +14,30 @@ altura = 480
 
 pygame.display.set_mode((largura, altura))
 pygame.display.set_caption ("Mazegen")
+
+
+#Random DFS
+
+G = nx.grid_2d_graph(20,20)
+
+
+#Instead of iterating through the neigbors it chooses one randomly
+def randomDFS(vertex):
+    G.nodes[vertex]['visited'] = 1
+    nextVertex = randUnvisitedNeighbor(vertex)
+
+    while nextVertex:
+        moveCell(vertex, nextVertex)
+        randomDFS(nextVertex)
+        nextVertex = randUnvisitedNeighbor(vertex)
+
+
+
+def createMaze():
+    startVertex = (0, 0)
+    randomDFS(startVertex)
+
+
 
 sair = True
 
